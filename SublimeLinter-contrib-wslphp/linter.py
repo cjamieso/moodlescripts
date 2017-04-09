78b8286341f9a4933483cdfb9a56b24359fccaf9
cjamieso <cjamieso@ualberta.ca>
2017-04-09 12:44:53 -0600

    fixing up php linter so that unexpected characters are flagged

--- SublimeLinter-contrib-wslphp/linter.py
+++ SublimeLinter-contrib-wslphp/linter.py
@@ -19,7 +19,7 @@ class Wslphp(Linter):
     syntax = ('php', 'html')
     regex = (
         r'^(?:Parse|Fatal) (?P<error>error):(\s*(?P<type>parse|syntax) error,?)?\s*'
-        r'(?P<message>(?:unexpected \'(?P<near>[^\']+)\')?.*) in - on line (?P<line>\d+)'
+        r'(?P<message>(?:unexpected \'(?P<near>[^\']+)\')?.*) in .* on line (?P<line>\d+)'
     )
     executable = 'wslphp'
 
