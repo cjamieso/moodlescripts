6041c7282775817c3a41f1a68b6272dfe6c0c517
Craig Jamieson <craig.jamieson@ualberta.ca>
2018-06-13 21:04:12 -0600

    Using new wslpath command as part of windows command files

--- php.cmd
+++ php.cmd
@@ -1,16 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
-call :convert %9 result
-set result1=!result!
-wsl php %1 %2 %3 %4=%5 %6 %7=%8 !result1!
+set winpath=%9
+set winpath=!winpath:\=\\!
+for /f "delims=" %%a in ('wsl wslpath !winpath!') do @set newpath=%%a
 
-:convert
-set fullpath=%1
-set letter=!fullpath:~0,1!
-set letter=!letter:C=c!
-set letter=!letter:D=d!
-set filename=!fullpath:~2!
-set filename=!filename:\=/!
-set result=/mnt/!letter!!filename!
-goto:eof
+wsl php %1 %2 %3 %4=%5 %6 %7=%8 !newpath!
