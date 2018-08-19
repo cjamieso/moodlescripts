9a4be17d124c1323de7f929fab0ec962a92fc98b
Craig Jamieson <craig.jamieson@ualberta.ca>
2018-08-19 16:53:21 -0600

    Updating some scripts for moodle 343

--- eslint.cmd
+++ eslint.cmd
@@ -4,5 +4,5 @@ setlocal enabledelayedexpansion
 set winpath=%5
 set winpath=!winpath:\=\\!
 for /f "delims=" %%a in ('wsl wslpath !winpath!') do @set newpath=%%a
-bash -c "~/gitprojects/moodle341/node_modules/.bin/eslint -c ~/gitprojects/moodle341/.eslintrc %1=%2 !newpath!"
+bash -c "~/gitprojects/moodle343/node_modules/.bin/eslint -c ~/gitprojects/moodle343/.eslintrc %1=%2 !newpath!"
 exit
