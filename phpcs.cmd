50b60b42a5e545d5c74baef5161b6d9973da89a5
Craig Jamieson <craig.jamieson@ualberta.ca>
2019-03-15 11:54:42 -0600

    Update to phpcs.cmd to not include standard - set directly in wsl instead. mdocker - password for moodle install change util - modified to allow hyphens in branch name

--- phpcs.cmd
+++ phpcs.cmd
@@ -1,16 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
-IF /I "%3%"=="--standard" (
-    set winpath=%6
-    set winpath=!winpath:\=\\!
-    for /f "delims=" %%a in ('wsl wslpath !winpath!') do @set newpath=%%a
-    wsl phpcs %1=%2 %3=%4 !newpath!
-)
-IF /I "%5%"=="--standard" (
-    set winpath=%4
-    set winpath=!winpath:\=\\!
-    for /f "delims=" %%a in ('wsl wslpath !winpath!') do @set newpath=%%a
-    wsl phpcs %1=%2 %5=%6 !newpath!
-)
+set winpath=%4
+set winpath=!winpath:\=\\!
+for /f "delims=" %%a in ('wsl wslpath !winpath!') do @set newpath=%%a
+wsl phpcs %1=%2 !newpath!
 exit
