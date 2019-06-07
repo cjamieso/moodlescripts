f14627354792e36e977006cabf8fe1e37d096773
Craig Jamieson <craig.jamieson@ualberta.ca>
2019-06-07 15:12:55 -0600

    Updating eslint.cmd path to 354

--- eslint.cmd
+++ eslint.cmd
@@ -4,5 +4,5 @@ setlocal enabledelayedexpansion
 set winpath=%5
 set winpath=!winpath:\=\\!
 for /f "delims=" %%a in ('wsl wslpath !winpath!') do @set newpath=%%a
-bash -c "~/gitprojects/moodle343/node_modules/.bin/eslint -c ~/gitprojects/moodle343/.eslintrc %1=%2 !newpath!"
+bash -c "~/gitprojects/moodle354/node_modules/.bin/eslint -c ~/gitprojects/moodle354/.eslintrc %1=%2 !newpath!"
 exit
