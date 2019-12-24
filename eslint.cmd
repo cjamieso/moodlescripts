c65bbd6f75babb9eb504ed9808e1fe30a7f5d2f9
Craig Jamieson <craig.jamieson@ualberta.ca>
2019-12-24 11:48:45 -0700

    Removed eslint (struggles in wsl)

--- eslint.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-
-set winpath=%5
-set winpath=!winpath:\=\\!
-for /f "delims=" %%a in ('wsl wslpath !winpath!') do @set newpath=%%a
-bash -c "~/gitprojects/moodle354/node_modules/.bin/eslint -c ~/gitprojects/moodle354/.eslintrc %1=%2 !newpath!"
-exit
