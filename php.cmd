e8426ad3f5efe6b62244d2e33c388660095727f7
Craig Jamieson <craig.jamieson@ualberta.ca>
2020-05-26 14:35:05 -0600

    Sublimelinter plugins no longer needed (as well as cmd files). Codechecker setup now handled in wsl setup file.

--- php.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-
-set winpath=%9
-set winpath=!winpath:\=\\!
-for /f "delims=" %%a in ('wsl wslpath !winpath!') do @set newpath=%%a
-
-wsl php %1 %2 %3 %4=%5 %6 %7=%8 !newpath!
--- phpcs.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-
-set winpath=%4
-set winpath=!winpath:\=\\!
-for /f "delims=" %%a in ('wsl wslpath !winpath!') do @set newpath=%%a
-wsl phpcs --standard=moodle !newpath!
-exit
